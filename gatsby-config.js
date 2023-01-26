6ac4418337dcbaad94276a743725e8fed77f48ef
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2023-01-26 11:29:52 -0500

    Added Tailwind CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Portfolio`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-web-font-loader", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-postcss", "gatsby-plugin-web-font-loader", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
