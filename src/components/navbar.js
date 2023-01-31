ea02db25b3984e0b8d032c3be48950249c5c5e40
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2023-01-31 13:10:46 -0500

    Branched out on the components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,1 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        About Me
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/education">
+                        Education
+                    </Link>
+                </li>
+                <li>
+                <Link to="/projects">
+                        Projects
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
