5e8913011067ff97a2adef44f6041069303e4b8e
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2023-02-01 14:50:19 -0500

    Working on links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 import "../style/site.scss"
 
 const IndexPage = () => {
@@ -6,10 +7,16 @@ const IndexPage = () => {
     <main>
         <div className="indexHeader">
           <h1 class="text-[#FC5185] font-semibold">MANH ANH TUAN NGUYEN</h1>
-          <h4 class="text-slate-400 font-light">Welcome to my web portfolio, enjoy your stay...</h4>
+          <h4 class="text-blue-300 font-light">Welcome to my web portfolio, enjoy your stay...</h4>
         </div>
-        <div>
-          
+        <div className="navigationBar">
+          <nav>
+            <ul>
+              <li>
+                  <Link to="/" class="text-slate-400">Home</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
     </main>
   )
