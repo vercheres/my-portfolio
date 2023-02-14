d698f055fe2298ed69e5afe57aae54ee04efec06
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2023-02-14 17:20:10 -0500

    Reformatting site.scss

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -58,10 +58,9 @@ const ProjectPage = () => {
                     {
                         projects && projects.map(project => {
                             console.log(project)
-                            const screenshot = project.screenshot
                             return (
-                                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                    <img class="rounded-t-lg" src={project.screenshot} alt="" />
+                                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" className="projectContainer">
+                                    <img class="rounded-t-lg" src={project.screenshot} alt=""/>
                                     <div class="p-5">
                                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
                                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{project.description}</p>
@@ -71,8 +70,8 @@ const ProjectPage = () => {
                                     <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                     </svg>
                                 </a>
+                                </div>
                             </div>
-                        </div>
 
                             )
                         })
